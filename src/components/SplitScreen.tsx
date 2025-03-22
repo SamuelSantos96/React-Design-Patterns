16b67790723f4c04cc605aa79e2ef94580d686b2
Samuel Santos <samuel.trabalho.96@gmail.com>
2025-03-22 13:34:39 +0000

    Fixed Tailwind Issue

--- src/components/SplitScreen.tsx
+++ src/components/SplitScreen.tsx
@@ -11,14 +11,17 @@ interface SplitScreenProps {
 export const SplitScreen = ({ children, leftWeight, rightWeight }: SplitScreenProps) => {
     const [left, right] = children;
 
+    const leftWidth = `${leftWeight}rem`;
+    const rightWidth = `${rightWeight}rem`;
+
     console.log("--- left", left);
 
     console.log("--- right", right);
 
     return (
         <section className='flex w-screen'>
-            <div>{left}</div>
-            <div>{right}</div>
+            <div style={{ width: leftWidth }}>{left}</div>
+            <div style={{ width: rightWidth }}>{right}</div>
         </section>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  }
 })
